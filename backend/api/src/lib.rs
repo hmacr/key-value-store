3d867bdffbb93edc6721c70d7eee464a67ccde30
hmacr <hmac.devo@gmail.com>
2023-08-23 18:33:45 +0530

    feat: enable CORS

--- backend/api/src/lib.rs
+++ backend/api/src/lib.rs
@@ -2,6 +2,9 @@ use axum::Router;
 use serde::{Deserialize, Serialize};
 use sqlx::{Pool, Postgres};
 use std::net::SocketAddr;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
 use tracing;
 
 mod health_check;
@@ -9,12 +12,18 @@ mod in_memory;
 mod postgres;
 
 pub async fn run_server(db: Pool<Postgres>) -> anyhow::Result<()> {
+    let middlewares = ServiceBuilder::new()
+        .layer(TraceLayer::new_for_http())
+        .layer(CorsLayer::permissive());
     let router = Router::new()
         .nest("/in_memory", in_memory::get_router())
         .nest("/postgres", postgres::get_router(db));
     let app = Router::new()
+        // .layer(CorsLayer::permissive())
+        // .layer(TraceLayer::new_for_http());
         .nest("/", health_check::get_router())
-        .nest("/api", router);
+        .nest("/api", router)
+        .layer(middlewares);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let server = axum::Server::bind(&addr).serve(app.into_make_service());
